4447d50fe225426f469f01b21daeb69a1e677689
Moatsy <024029@lths.org>
2015-12-15 09:33:02 -0600

    Update TFM_Command.java

--- src/me/StevenLawson/TotalFreedomMod/Commands/TFM_Command.java
+++ src/me/StevenLawson/TotalFreedomMod/Commands/TFM_Command.java
@@ -14,7 +14,7 @@ import org.bukkit.entity.Player;
 
 public abstract class TFM_Command
 {
-    public static final String MSG_NO_PERMS = ChatColor.YELLOW + "You do not have permission to use this command.";
+    public static final String MSG_NO_PERMS = ChatColor.RED + "You do not have permission to use this command.";
     public static final String YOU_ARE_OP = ChatColor.YELLOW + "You are now op!";
     public static final String YOU_ARE_NOT_OP = ChatColor.YELLOW + "You are no longer op!";
     public static final String NOT_FROM_CONSOLE = "This command may not be used from the console.";
