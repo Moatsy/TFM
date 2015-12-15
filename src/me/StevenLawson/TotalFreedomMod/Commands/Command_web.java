31da6e10c800d3fe3268e9c2fc270ce9a4e80114
Moatsy <024029@lths.org>
2015-12-15 09:49:32 -0600

    Create Command_web.java

---
+++ src/me/StevenLawson/TotalFreedomMod/Commands/Command_web.java
@@ -0,0 +1,19 @@
+package me.StevenLawson.TotalFreedomMod.Commands;
+
+import java.util.ArrayList;
+import java.util.List;
+import me.StevenLawson.TotalFreedomMod.TFM_AdminList;
+import me.StevenLawson.TotalFreedomMod.TFM_PlayerRank;
+import me.StevenLawson.TotalFreedomMod.TFM_Util;
+import org.apache.commons.lang3.StringUtils;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@CommandPermissions(level = AdminLevel.ALL, source = SourceType.BOTH)
+@CommandParameters(description = "Tell you are website", usage = "/<command>", aliases = "site")
+public class Command_web extends TFM_Command
+//                                //
+// More Work Will Be Done To This //
+//                                //
